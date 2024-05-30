7677f3440d035bf5e9504ebf224d57ad31655522
菜鸟王 <24060287689@qq.com>
2024-05-30 15:06:51 +0800

    新建仓库

---
+++ src/main/java/com/dept01/bitfleamarket/BitFleaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dept01.bitfleamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitFleaMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitFleaMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dept01/bitfleamarket/BitFleaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dept01.bitfleamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitFleaMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
