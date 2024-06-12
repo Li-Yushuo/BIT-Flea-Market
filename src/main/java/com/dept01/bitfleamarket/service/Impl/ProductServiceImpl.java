4bb19e1da84585c13acdf605856a7bf11cef1579
菜鸟王 <24060287689@qq.com>
2024-06-12 09:05:28 +0800

    2024.6.12 获取用户商品添加了product_id返回

--- src/main/java/com/dept01/bitfleamarket/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/dept01/bitfleamarket/service/Impl/ProductServiceImpl.java
@@ -219,6 +219,7 @@ public class ProductServiceImpl implements ProductService {
             String coverImageUrl = images.isEmpty() ? "null" : images.get(0).getImageUrl();
 
             return new ProductResponse(
+                    product.getProductId().toString(),
                     product.getName(),
                     product.getPrice(),
                     product.getPurchaseMethod(),
